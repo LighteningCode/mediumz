1b02c78f2ebb3e50640fbe30ba9303cd145a5955
LighteningCode <elvisagbesi@gmail.com>
2022-01-21 19:34:24 +0000

    feat: completed header section

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,46 +15,21 @@ const Home: NextPage = () => {
 
       <Navbar />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <section className='flex flex-row  bg-yellow-400 border-b border-black'>
+        <div className="w-1/2 px-14 py-12">
+          <div>
+            <h2 className='text-7xl'>Mediumz is a place to read,copy,and remake</h2>
+            <p className='mb-10 mt-2 font-semibold text-stone-800'>It's easy and free to post your thinking on any topic and connect with millions of readers.</p>
+            <span className="px-5 py-3 bg-white cursor-pointer rounded-full text-base font-thin text-stone-800 border border-stone-800">Start Writing</span>
+          </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-      </main>
+        <div className="w-1/2 relative">
+          <div className="absolute  -bottom-1.5 right-20">
+            <Image src="/comma.png" alt="Comma" width={400} height={350} />
+          </div>
+        </div>
+      </section>
 
       <footer className={styles.footer}>
         <a
