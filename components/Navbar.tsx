ae8a5a83f691c3962f3f0220730533510bce5377
LighteningCode <elvisagbesi@gmail.com>
2022-02-03 18:55:33 +0000

    feat: nav bar can inject classes now

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 export default function Navbar(props: any) {
   return (
-   <nav className="flex flex-row justify-between py-5 border-b border-black">
+   <nav className={`flex flex-row justify-between py-5 border-b bg-white border-black ${props.className ? props.className : ""}`}>
      <div className="flex flex-row pl-20">
        <h2 className="text-3xl font-bold">Mediumz</h2>
      </div>
