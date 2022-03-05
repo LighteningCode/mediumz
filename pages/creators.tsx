c2dfcfb302c00f372b8421fbd278f43438299c3f
LighteningCode <elvisagbesi@gmail.com>
2022-03-05 14:13:04 +0000

    feat: completed creators screen

--- pages/creators.tsx
+++ pages/creators.tsx
@@ -2,6 +2,38 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Navbar from "../components/Navbar";
 import Footer from '../components/Footer';
+import { useState } from 'react';
+
+const faqdata = [
+    {
+        question: "How do I start writing on Medium?",
+        answer: "First, make a free account. Then, to create a story, click on your profile picture in the top-right corner of the page, then “Write a Story.”"
+    },
+    {
+        question: "How can I make money writing on Mediumz?",
+        answer: "If you join the Medium Partner Program, you can earn money based on how much time members spend reading your work and whenever you convert non-paying readers into members."
+    },
+    {
+        question: "Who writes on Mediumz?",
+        answer: "Anyone can write on Medium. Whether you’ve never written before or are ready to create a full publication, it’s easy to get started and we allow you to focus more on big ideas and less on driving clicks. With the option to earn for your work, you can also be directly rewarded for the value you provide readers."
+    },
+    {
+        question: "Do I need to be in the Partner Program to write on Medium?",
+        answer: "No. You can write on Medium even if you are not in the Partner Program. If you are in the Partner Program you can also choose which of the stories you publish are eligible for the Program. For more about the Partner Program, click here."
+    },
+    {
+        question: "What can I write about on Medium?",
+        answer: "Medium’s designed for short stories that make someone’s day better, manifestos that change the world, and everything in between. It’s used by everyone from professional journalists to data scientists to amateur cooks. Whatever the topic, Medium helps you find the right audience for what you have to say."
+    },
+    {
+        question: "Who can read the work I publish on Medium?",
+        answer: "Anyone on the internet can read your stories. Even if your story is being distributed in the Partner Program as a part of our metered paywall, anyone can still read a limited number of articles for free each month. If you want to share an un-metered version of your story, you can use a Friend Link."
+    },
+    {
+        question: "How can I grow my audience with Medium?",
+        answer: "Medium has a number of tools to connect you with over 100 million readers on the platform and beyond. Some ways you can build your audience are by starting a newsletter, submitting to publications, following the distribution standards, and sharing your stories on social media. We also encourage you to check out our blog, 3 Minute Read, for more helpful tips and tricks – like how to write a great headline and SEO tips to make your stories more discoverable."
+    },
+]
 
 
 const Writer = ({ image, name, link, index, portfolio }: any) => {
@@ -16,12 +48,25 @@ const Writer = ({ image, name, link, index, portfolio }: any) => {
     )
 }
 
-const Question = ({ image, question, link, index, portfolio }: any) => {
+const Question = ({ image, question, answer, link, index, portfolio }: any) => {
+
+    const [show, setShow] = useState(false);
+
     return (
-        <div className={`flex flex-row h-26 w-full border-white border-b text-white px-10 py-2`}>
-            <p className="font-medium my-8 text-lg leading-6 self-center">{question || "Default Question"}</p>
+        <div onClick={() => setShow(!show)} className="flex flex-col transition-all py-5  cursor-pointer border-white border-b">
+            <div className={`flex flex-row h-26 w-full  text-white px-20 mb-8 `}>
+                <p className="font-medium  text-xl leading-6 self-center hover:underline">{question || "Default Question"}</p>
+            </div>
+
+            {
+                show &&
+                <div className="px-20 ">
+                    <p className="font-medium mb-4 text-base leading-6 self-center"> {answer || "No answer"}</p>
+                </div>
+            }
 
         </div>
+
     )
 }
 
@@ -37,16 +82,17 @@ const Creators: NextPage = () => {
             <Navbar className="fixed top-0 left-0 w-full z-50" />
 
             <main className="flex flex-col">
-                <div className="flex flex-col bg-red-500 border-b border-black pt-20">
+                <div style={{ backgroundColor: "#F24D2E" }} className="flex flex-col bg-red-500 border-b border-black pt-20">
 
                     <div className="flex flex-row">
-                        <div className="flex flex-col w-3/5 pl-6 text-black">
-                            <h1 className="text-8xl font-medium tracking-tight mb-14">Publish, grow, and earn, all in one place.</h1>
-                            <p className="font-medium my-8 text-lg leading-6 self-center">If you have a story to tell, knowledge to share, or a perspective to offer — welcome home. Here, your writing can thrive in a network supported by millions of readers — not ads.</p>
+                        <div className="flex flex-col w-8/12 pl-6 pt-10 text-black">
+                            <p style={{ letterSpacing: "10px" }} className="font-medium text-sm uppercase tracking-widest leading-6 text-white">Start a blog today</p>
+                            <h1 className="text-8xl font-medium tracking-tight mb-5">Publish, grow, <br /> and earn, all in <br /> one place.</h1>
+                            <p className="font-medium my-8 text-lg leading-6">If you have a story to tell, knowledge to share, or a perspective <br /> to offer — welcome home. Here, your writing can <br /> thrive in a network supported by millions of readers — not ads.</p>
                             <button className="border-white bg-white text-black hover:text-white hover:bg-transparent transition-all duration-150  border w-64 text-xl rounded-full px-3 py-2 mb-16">Start Writing</button>
                         </div>
-                        <div className="w-2/5 border-l border-black">
-                            <img src="https://cdn-static-1.medium.com/sites/medium.com/about/images/SupportWriters.svg" width="250" height="135" alt=""></img>
+                        <div className="w-4/12 border-l border-black pt-10">
+                            <img src="./creators.png" className="h-auto w-full"></img>
                         </div>
                     </div>
 
@@ -57,7 +103,7 @@ const Creators: NextPage = () => {
                         </div>
                         <div className="border-stone-800 w-1/2 py-10 px-10">
                             {[0, 0, 0, 0, 0, 0].map((value, idx) =>
-                                <Writer index={idx} />
+                                <Writer key={idx} index={idx} />
                             )
                             }
                         </div>
@@ -142,8 +188,8 @@ const Creators: NextPage = () => {
                 <div className="flex flex-col bg-black text-white relative border-black">
                     <h1 className="text-5xl px-10 font-medium tracking-tight mb-28 mt-8">More about writing on Medium:</h1>
 
-                    {[0, 0, 0, 0, 0, 0].map((value, idx) =>
-                        <Question index={idx} />
+                    {faqdata.map((value, idx) =>
+                        <Question key={idx} index={idx} {...value} />
                     )}
 
                     <div className="flex flex-row justify-center my-7">
