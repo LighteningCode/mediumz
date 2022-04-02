f542c9b01bf64af8732166994bee0ccf699c4c6c
LighteningCode <elvisagbesi@gmail.com>
2022-04-02 10:40:23 +0000

    feat: mobile navbar implemented

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,20 +4,30 @@ import Link from "next/link";
 export default function Navbar(props: any) {
 
   return (
-   <nav className={`flex flex-row justify-between py-5 border-b transition-all duration-300 border-black ${props.className ? props.className : ""}`}>
-     <div className="flex flex-row pl-20 mobile:pl-4">
-       <Link href="/"><h2 className="text-3xl font-bold cursor-pointer">Mediumz</h2></Link>
-     </div>
+    <nav className={`${props.className ? props.className : ""}`}>
+      <nav className={`flex flex-row justify-between py-5 mobile:py-4 border-b transition-all duration-300 border-black `}>
+        <div className="flex flex-row pl-20 mobile:pl-4">
+          <Link href="/"><h2 className="text-3xl font-bold cursor-pointer">Mediumz</h2></Link>
+        </div>
 
-     <div className="pr-20 mobile:pr-4 self-center flex flex-row">
-       <ul className="flex flex-row justify-between mobile:hidden">
-         <li className="mx-3 font-thin self-center text-sm"><Link href="/about">Our story</Link></li>
-         <li className="mx-3 font-thin self-center text-sm"><Link href="/membership">Membership</Link></li>
-         <li className="mx-3 font-thin self-center text-sm"><Link href="/creators">Write</Link></li>
-         <li className="mx-3 font-thin self-center text-sm"><Link href="/signin">Sign In</Link></li>
-       </ul>
-       <span className="px-4 py-2 bg-stone-900 cursor-pointer hover:bg-black rounded-full text-sm font-thin ml-5 text-white">Get Started</span>
-     </div>
-   </nav>     
+        <div className="pr-20 mobile:pr-4 self-center flex flex-row">
+          <ul className="flex flex-row justify-between mobile:hidden">
+            <li className="mx-3 font-thin self-center text-sm"><Link href="/about">Our story</Link></li>
+            <li className="mx-3 font-thin self-center text-sm"><Link href="/membership">Membership</Link></li>
+            <li className="mx-3 font-thin self-center text-sm"><Link href="/creators">Write</Link></li>
+            <li className="mx-3 font-thin self-center text-sm"><Link href="/signin">Sign In</Link></li>
+          </ul>
+          <span className="px-4 py-2 bg-stone-900 cursor-pointer hover:bg-black rounded-full text-sm font-thin ml-5 text-white">Get Started</span>
+        </div>
+      </nav>
+
+      <ul className="flex-row hidden justify-between mobile:flex py-3 border-black border-b">
+        <li className="mx-3 font-normal self-center text-sm"><Link href="/about">Our story</Link></li>
+        <li className="mx-3 font-normal self-center text-sm"><Link href="/membership">Membership</Link></li>
+        <li className="mx-3 font-normal self-center text-sm"><Link href="/creators">Write</Link></li>
+        <li className="mx-3 font-normal self-center text-sm"><Link href="/signin">Sign In</Link></li>
+      </ul>
+    </nav>
+
   )
 }
